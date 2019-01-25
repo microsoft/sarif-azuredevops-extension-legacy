a40385017713ce441cc03fffc2ee63c3442ec671
Jeff King <jeffking@gmail.com>
2019-01-24 16:51:20 -0800

    Fully use async/await, and...
    - Feed promise<json> to the viewer.
    - Remove loading state (viewer will handle).

--- index.tsx
+++ index.tsx
@@ -4,61 +4,46 @@ import 'script-loader!vss-web-extension-sdk/lib/VSS.SDK.min.js'
 import 'script-loader!./zip/zip.js'
 import {ResultsViewer, Dropdown} from 'sarif-web-component/Index.tsx'
 
-const ensureFileLoaded = async file => {
-	file.json = file.json || await new Promise(resolve => {
-		file.getData(
-			new zip.TextWriter(),
-			text => resolve(text),
-			(current, total) => {}
-		)
-	})
-}
 declare var zip: any, VSS: any
+const promisify = (f, ...args) => new Promise(r => f(...args, r)) as any
 
 class Tab extends React.Component<any, any> {
-	state = { loading: true, files: [], fileIndex: 0 }
+	state = { files: undefined, fileIndex: 0 }
 	constructor(props) {
 		super(props)
-		VSS.init()
+		VSS.init({ explicitNotifyLoaded: true })
 		VSS.require(['TFS/Build/RestClient'], restClient => {
 			const client = restClient.getClient()
-			const config = VSS.getConfiguration()
-			config.onBuildChanged(async build => {
+			const onBuildChanged = async build => {
 				const artifacts = await client.getArtifacts(build.id)
-				if (artifacts.filter(a => a.name === 'CodeAnalysisLogs').length) {
+				const files = await (async () => {
+					if (!artifacts.some(a => a.name === 'CodeAnalysisLogs')) return []
 					const logsZip = await client.getArtifactContentZip(build.id, 'CodeAnalysisLogs')
-					const blob = new Blob([new Uint8Array(logsZip)])
-					zip.createReader(new zip.BlobReader(blob),
-						reader => {
-							reader.getEntries(async entries => {
-								const files = entries.filter(entry => entry.filename.endsWith('.sarif'))
-								if (files.length) await ensureFileLoaded(files[0])
-								this.setState({ loading: false, files })
-								// reader.close(() => {})
-							})
-						},
-						error => { debugger }
-					)
-				} else {
-					this.setState({ loading: false })
-				}
-			})
+					const reader = await promisify(zip.createReader, new zip.BlobReader(new Blob([new Uint8Array(logsZip)])))
+					const entries = await promisify(reader.getEntries.bind(reader))
+					return entries
+						.filter(entry => entry.filename.endsWith('.sarif'))
+						.map((entry, i) => {
+							let p = undefined
+							return {
+								key: i, text: entry.filename.replace('CodeAnalysisLogs/', ''),
+								sarif: () => p = p || promisify(entry.getData.bind(entry), new zip.TextWriter())
+							}
+						})
+				})()
+				this.setState({ files })
+				VSS.notifyLoadSucceeded()
+			}
+			VSS.getConfiguration().onBuildChanged(onBuildChanged) // ;onBuildChanged({ id: 75 })
 		})
 	}
 	render() {
-		const {files, fileIndex, loading} = this.state
+		const {files, fileIndex} = this.state
 		const dd = <Dropdown className="resultsDropdown"
-			options={files.map((f, i) => ({ key: i, text: f.filename.replace('CodeAnalysisLogs/', '') }))}
-			selectedKey={fileIndex}
-			onChange={async (ev, option, i) => {
-				const file = files[i]
-				await ensureFileLoaded(file)
-				this.setState({ fileIndex: i })
-			}} />
-		
-		if (loading) return <div className="full">Loading...</div>
-		return files.length && files[fileIndex].json
-			? <ResultsViewer sarif={files[fileIndex].json} prefix={dd} />
+			options={files} selectedKey={fileIndex}
+			onChange={(ev, option, i) => this.setState({ fileIndex: i })} />
+		return !files || files.length
+			? <ResultsViewer sarif={files && files[fileIndex].sarif()} prefix={dd} />
 			: <div className="full">No SARIF artifacts found.</div>
 	}
 }
