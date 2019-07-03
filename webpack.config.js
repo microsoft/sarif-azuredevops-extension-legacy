d7489fa128e086d2f96ce5ac807eddb88f97df6c
Jeff King <jeffking@gmail.com>
2019-07-03 16:13:10 -0700

    Modernize configs.

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,35 @@ const path = require("path")
 const webpack = require('webpack')
 
 module.exports = {
-    entry: "./index.tsx",
-    output: { path: __dirname, filename: "bundle.js" },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        alias: {
-            'react': path.resolve('node_modules/react'),
-            'react-dom': path.resolve('node_modules/react-dom'),
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                exclude: /node_modules/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
-        ]
-    },
-    devServer : { port: 8080, https: true }
+	entry: "./index.tsx",
+	output: { path: __dirname, filename: "bundle.js" },
+	mode: 'production',
+	resolve: {
+		extensions: [".js", ".jsx", ".ts", ".tsx"],
+		alias: {
+			'react': path.resolve('node_modules/react'),
+			'react-dom': path.resolve('node_modules/react-dom'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{ test: /\.png$/, use: 'url-loader' },
+			{ test: /\.woff$/, use: 'url-loader' },
+		]
+	},
+	devServer : {
+		host: '0.0.0.0', // Neccesary to server outside localhost
+		port: 8080,
+		stats: 'none',
+		https: true,
+	}
 }
