12eac007e1b359299fe0d6abc3a6fb465092370d
Jeff King <jeffking@gmail.com>
2019-10-31 15:45:59 -0700

    Plumb pipelineId.

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {observable} from 'mobx'
+import {observable, runInAction} from 'mobx'
 import {observer} from 'mobx-react'
 
 import {AppInsights} from "applicationinsights-js"
@@ -17,6 +17,7 @@ const perfLoadStart = performance.now() // For telemetry.
 
 @observer class Tab extends React.Component {
 	@observable.ref logs = undefined as Log[]
+	@observable pipelineId = undefined as string
 	constructor(props) {
 		super(props)
 		VSS.init({
@@ -33,63 +34,67 @@ const perfLoadStart = performance.now() // For telemetry.
 			const onBuildChanged = build => {
 				;(async () => { // Wrapper IIFE to allow rejection to be caught by our own telemetry.
 					// Otherwise consumed by: /WebPlatform/Web/extensions/vss-web/vss-platform/Trace.ts
-				const artifacts = await client.getArtifacts(build.id, build.project.id)
-				const files = await (async () => {
-					if (!artifacts.some(a => a.name === 'CodeAnalysisLogs')) return []
-					const arrayBuffer = await client.getArtifactContentZip(build.id, 'CodeAnalysisLogs', build.project.id)					
-					const zip = await JSZip.loadAsync(arrayBuffer)
-					return Object.values<any>(zip.files)
-						.filter(entry => !entry.dir && entry.name.endsWith('.sarif'))
-						.map((entry, i) => {
-							let cachedPromise = undefined as string
-							return {
-								key:   i,
-								text:  entry.name.replace('CodeAnalysisLogs/', ''),
-								sarif: () => cachedPromise = cachedPromise || entry.async('string') as string
-							}
-						})
-				})()
+					const artifacts = await client.getArtifacts(build.id, build.project.id)
+					const files = await (async () => {
+						if (!artifacts.some(a => a.name === 'CodeAnalysisLogs')) return []
+						const arrayBuffer = await client.getArtifactContentZip(build.id, 'CodeAnalysisLogs', build.project.id)					
+						const zip = await JSZip.loadAsync(arrayBuffer)
+						return Object.values<any>(zip.files)
+							.filter(entry => !entry.dir && entry.name.endsWith('.sarif'))
+							.map((entry, i) => {
+								let cachedPromise = undefined as string
+								return {
+									key:   i,
+									text:  entry.name.replace('CodeAnalysisLogs/', ''),
+									sarif: () => cachedPromise = cachedPromise || entry.async('string') as string
+								}
+							})
+					})()
 
-				const logTexts = await Promise.all(files.map(async file => await file.sarif()))
-				const logs = logTexts.map(log => JSON.parse(log) as Log)
+					const logTexts = await Promise.all(files.map(async file => await file.sarif()))
+					const logs = logTexts.map(log => JSON.parse(log) as Log)
 
-				// Show file names when the tool names are homogeneous.
-				if (files.length > 1) {
+					// Show file names when the tool names are homogeneous.
+					if (files.length > 1) {
 						const toolNames = logs.map(log => {
 							return log.runs
 								.filter(run => run.tool.driver) // Guard against old versions.
 								.map(run => run.tool.driver.name)
 						})
-					const toolNamesSet = new Set([].concat(...toolNames))
-					if (toolNamesSet.size === 1) {
-						logs.forEach((log, i) => 
-							log.runs.forEach(run => {
-								run.properties = run.properties || {}
-								run.properties['logFileName'] = files[i].text
-							})
-						)
+						const toolNamesSet = new Set([].concat(...toolNames))
+						if (toolNamesSet.size === 1) {
+							logs.forEach((log, i) => 
+								log.runs.forEach(run => {
+									run.properties = run.properties || {}
+									run.properties['logFileName'] = files[i].text
+								})
+							)
+						}
 					}
-				}
 
-				this.logs = logs
-				VSS.notifyLoadSucceeded()
+					runInAction(() => {
+						this.logs = logs
+						this.pipelineId = `${VSS.getWebContext().account.name}.${build.definition.id}`
+					})
+					
+					VSS.notifyLoadSucceeded()
 
-				if (isProduction) {
-					AppInsights.trackPageView(wc.project.name, document.referrer, undefined, undefined, performance.now() - perfLoadStart)
-				}
+					if (isProduction) {
+						AppInsights.trackPageView(wc.project.name, document.referrer, undefined, undefined, performance.now() - perfLoadStart)
+					}
 				})()
 			}
 			VSS.getConfiguration().onBuildChanged(onBuildChanged) // ;onBuildChanged({ id: 334, project: { id: '185a21d5-2948-4dca-9f43-a9248d571bd3' } })
 		})
 	}
 	render() {
-		const {logs} = this
+		const {logs, pipelineId} = this
 		const filterState = {
 			Baseline: { value: ['new', 'updated', 'absent'] },
 			Level: { value: ['error'] },
 		}
 		return !logs || logs.length
-			? <Viewer logs={logs} filterState={filterState} />
+			? <Viewer logs={logs} pipelineId={pipelineId} filterState={filterState} />
 			: <div className="full">No SARIF artifacts found.</div>
 	}
 }
